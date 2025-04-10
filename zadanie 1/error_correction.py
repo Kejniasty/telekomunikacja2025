8c8b568fa7ebcda68ec55d52d2d08b1e9f01adde
Kejniasty <kejniasty@gmail.com>
2025-04-10 03:15:07 +0200

    komentarze

--- zadanie 1/error_correction.py
+++ zadanie 1/error_correction.py
@@ -19,27 +19,29 @@ def kodowanie(tekst):
     wynik = []
     for znak in tekst:
         bity_danych = []
-        for bit in format(ord(znak), '08b'):
+        for bit in format(ord(znak), '08b'): # zamiana liter na indeks z ASCII po czym na liczbę binarną
             bity_danych.append(int(bit))
         bity_parzystosci = (np.dot(H[:, :8], bity_danych) % 2).tolist() # (polowa Hamminga * bity) modulo 2 każdego elementu
-        wynik.append(bity_danych + bity_parzystosci)
+        wynik.append(bity_danych + bity_parzystosci) # połączone wektory danych i parzystosci dodajemy do wynikowej macierzy
     return wynik
 
 def sprawdz_poprawnosc(wiadomosc):
     odkodowana = []
     for slowo in wiadomosc:
         syndrom = np.dot(H, slowo) % 2
-        if not any(syndrom):
+        if not any(syndrom): # jezeli wszystkie wyrazy w syndromie == 0 to w słowie nie ma błędów
             odkodowana.append(slowo)
             continue
 
+        # szukanie jednego błędu
         znaleziony = False
-        for i in range(len(H[0])):
-            if np.array_equal(H[:, i], syndrom):
+        for i in range(len(H[0])): # dla każdej kolumny
+            if np.array_equal(H[:, i], syndrom): # jeżeli kolumna jest równa syndromowi, to i jest indeksem błędu
                 slowo[i] = 1 - slowo[i]
-                znaleziony = True
+                znaleziony = True # ustawiamy flagę żeby nie szukać dwóch błędów
                 break
 
+        #szukanie dwóch błędów
         if not znaleziony:
             for i, j in combinations(range(len(H[0])), 2): # dla różnych kombinacji dwóch indeksow
                 suma_kolumn = (H[:, i] + H[:, j]) % 2 # sumujemy dwie kolumny
@@ -55,8 +57,7 @@ def dekodowanie(odkodowana):
     tekst = ''
     for slowo in odkodowana:
         number = 0
-        for bit_index in range(8):
-            number += slowo[bit_index] * bin_to_dec[bit_index]
-        tekst += chr(number)
-
+        for bit_index in range(8): # dla pierwszych 8 bitów słowa
+            number += slowo[bit_index] * bin_to_dec[bit_index] # konwertujemy binarne wartości na decymalne
+        tekst += chr(number) # dodajemy literę do tekstu wynikowego
     return tekst
--- zadanie 1/main.py
+++ zadanie 1/main.py
@@ -1,6 +1,7 @@
 import error_correction as ec
 import random
 
+# funkcja do konwersji macierzy 2D na string
 def make_string(matrix):
     temp_string = ''
     for word in matrix:
@@ -12,58 +13,72 @@ def make_string(matrix):
 # mode - a = kodowanie, b = dekodowanie, c = kodowanie -> dekodowanie
 def file_handling(input_name: str, output_name: str, errors: int, mode='a', check_errors='a') -> None:
     match mode:
+        # kodowanie tekstu z pliku do pliku
         case 'a':
             with open(input_name, 'r') as input_file:
                 encoded = []
                 print("===================\nOdczytana wiadomość\n===================")
                 for line in input_file:
                     print(line, end='')
-                    encoded += ec.kodowanie(line)
+                    encoded += ec.kodowanie(line) # do macierzy dodajemy kolejne zakodowane linie
                 print()
 
-                error_byte = random.randint(0, len(encoded))
-                for i in range(errors):
-                    error_bit = random.randint(0, 15)
-                    encoded[error_byte][error_bit] = 1 - encoded[error_byte][error_bit]
-                    print(f"Ups! na {16 - error_bit} bicie {error_byte + 1} słowa wkradł się błąd!")
+            #losujemy w którym słowie wystąpi błąd i zależnie od wybranej ilości błędów psujemy "errors" bitów
+            error_byte = random.randint(0, len(encoded))
+            for i in range(errors):
+                error_bit = random.randint(0, 15)
+                encoded[error_byte][error_bit] = 1 - encoded[error_byte][error_bit]
+                print(f"Ups! na {16 - error_bit} bicie {error_byte + 1} słowa wkradł się błąd!")
 
-                temp_string = make_string(encoded)
-                print("====================\nZakodowana wiadomość\n====================")
-                print(temp_string)
+            # zamieniamy macierz z błędem na stringa
+            temp_string = make_string(encoded)
+            print("====================\nZakodowana wiadomość\n====================")
+            print(temp_string)
 
+            # którego wpisujemy do pliku
             with open(output_name, 'w') as output_file:
                 output_file.write(temp_string)
 
+        # dekodowanie z pliku do pliku
         case 'b':
+            # w pliku słowa są oddzielone spacjami, dlatego korzystamy ze .split()
             with open(input_name, 'r') as input_file:
                 line = input_file.readline()
-                line = line.strip('\n')
-                line = line.split(' ')
-                encoded = []
-                for i in range(len(line)):
-                    encoded.append([])
-                    for bit in line[i]:
-                        encoded[i].append(int(bit))
-                if check_errors == 'a':
-                    encoded = ec.sprawdz_poprawnosc(encoded)
-                    print("====================\nWiadomość bez błędu\n====================")
-                    print(make_string(encoded))
-                text = ec.dekodowanie(encoded)
+            line = line.strip('\n')
+            line = line.split(' ')
+            # konwertujemy poszczególne bity macierzy na liczby całkowite
+            encoded = []
+            for i in range(len(line)):
+                encoded.append([])
+                for bit in line[i]:
+                    encoded[i].append(int(bit))
+            if check_errors == 'a':
+                # korygujemy błędy w macierzy
+                encoded = ec.sprawdz_poprawnosc(encoded)
+                print("====================\nWiadomość bez błędu\n====================")
+                print(make_string(encoded))
+            # dekodujemy macierz
+            text = ec.dekodowanie(encoded)
 
             with open(output_name, 'w') as output_file:
                 print("===================\nOdczytana wiadomość\n===================")
                 print(text)
+                # wpisujemy ją do pliku
                 output_file.write(text)
 
+        # kodowanie, dekodowanie i zapis tego do pliku
         case 'c':
             print("===================\nOdczytana wiadomość\n===================")
+            # odczytujemy zawartość pliku i kodujemy go linia po linii
             encoded_file = []
             with open(input_name, 'r') as input_file:
                 for line in input_file:
                     print(line, end='')
+                    # dodajemy każdą linię do macierzy
                     encoded_file += ec.kodowanie(line)
             print()
 
+            # w wylosowanym słowie psujemy macierz
             error_byte = random.randint(0, len(encoded_file))
             for i in range(errors):
                 error_bit = random.randint(0, 15)
@@ -72,18 +87,22 @@ def file_handling(input_name: str, output_name: str, errors: int, mode='a', chec
 
 
             print("====================\nZakodowana wiadomość\n====================")
+            # zamieniamy macierz na string, żeby ją wyświetlić w ładnej formie
             temp_string = make_string(encoded_file)
             print(temp_string)
 
             if check_errors == 'a':
+                # korygujemy błędy w macierzy
                 encoded_file = ec.sprawdz_poprawnosc(encoded_file)
                 print("====================\nWiadomość bez błędu\n====================")
                 print(make_string(encoded_file))
+            # macierz dekodujemy
             text = ec.dekodowanie(encoded_file)
 
             print("====================\nOdkodowana wiadomość\n====================")
             with open(output_name, 'w') as output_file:
                 print(text)
+                # zdekodowany tekst wpisujemy do pliku
                 output_file.write(text)
 
 
@@ -92,59 +111,68 @@ def file_handling(input_name: str, output_name: str, errors: int, mode='a', chec
 # mode - a = kodowanie, b = dekodowanie, c = kodowanie -> dekodowanie
 def keyboard_handling(text, errors, mode='a', check_errors='a'):
     match mode:
+        # kodowanie tekstu z klawiatury na ekran
         case 'a':
+            # kodujemy linię tekstu
             encoded_line = ec.kodowanie(text)
 
+            # w wylosowanym słowie psujemy macierz
             error_byte = random.randint(0, len(encoded_line))
             for i in range(errors):
                 error_bit = random.randint(0, 15)
                 encoded_line[error_byte][error_bit] = 1 - encoded_line[error_byte][error_bit]
                 print(f"Ups! na {16 - error_bit} bicie {error_byte + 1} słowa wkradł się błąd!")
 
+            # zamieniamy macierz na tekst i wyświetlamy ją na ekranie
             temp_string = make_string(encoded_line)
             print("====================\nZakodowana wiadomość\n====================")
             print(temp_string)
 
+        # dekodowanie tekstu z klawiatury na ekran
         case 'b':
             line = text.strip('\n')
             line = line.split(' ')
+            # konwetujemy stringa na macierz intów
             encoded = []
             for i in range(len(line)):
                 encoded.append([])
                 for bit in line[i]:
                     encoded[i].append(int(bit))
 
-
             if check_errors == 'a':
+                # korygujemy błędy w macierzy
                 encoded = ec.sprawdz_poprawnosc(encoded)
                 print("====================\nWiadomość bez błędu\n====================")
                 print(make_string(encoded))
 
-            temp_string = make_string(encoded)
-
-            print("====================\nZakodowana wiadomość\n====================")
-            print(temp_string)
+            # dekodujemy tekst i wyświetlamy go na ekranie
             text = ec.dekodowanie(encoded)
             print("====================\nOdkodowana wiadomość\n====================")
             print(text)
 
+        # kodowanie + dekodowanie tekstu z klawiatury na ekran
         case 'c':
+            # kodujemy tekst od użytkownika
             encoded_line = ec.kodowanie(text)
 
+            # psujemy macierz w losowym słowie "errors" razy
             error_byte = random.randint(0, len(encoded_line))
             for i in range(errors):
                 error_bit = random.randint(0, 15)
                 encoded_line[error_byte][error_bit] = 1 - encoded_line[error_byte][error_bit]
                 print(f"Ups! na {16 - error_bit} bicie {error_byte + 1} słowa wkradł się błąd!")
 
-            temp_string = make_string(encoded_line)
+            # wyświetlamy zakodowaną wiadomość
             print("====================\nZakodowana wiadomość\n====================")
-            print(temp_string)
+            print(make_string(encoded_line))
 
             if check_errors == 'a':
+                # korygujemy błędy w macierzy
                 encoded_line = ec.sprawdz_poprawnosc(encoded_line)
                 print("====================\nWiadomość bez błędu\n====================")
                 print(make_string(encoded_line))
+
+            # dekodujemy macierz i wyświetlamy ją na ekranie
             text = ec.dekodowanie(encoded_line)
             print("====================\nOdkodowana wiadomość\n====================")
             print(text)
